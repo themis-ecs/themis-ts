b68f03b36f3d00718e5e74cee4c913d2128381c1
Christopher Bartel <christopher.bartel@cologne-intelligence.de>
2021-08-21 17:13:26 +0200

    code docs

--- src/public/world.ts
+++ src/public/world.ts
@@ -2,18 +2,81 @@ import { Entity } from './entity';
 import { BlueprintDefinition } from './blueprint';
 import { Event, EventErrorCallback, EventListener, EventType } from './event';
 
+/**
+ * The world is part of the main API of themis-ecs. It is the place, where your systems, your components and
+ * your entities live in. You can use this object to create new entities, register and use blueprints, register
+ * and submit events and most importantly: you use the update method to let time pass in your world.
+ */
 export interface World {
+  /**
+   * Iterate your world, this will cause all systems to update
+   * @param dt the time that has passed since the last update
+   */
   update(dt: number): void;
+
+  /**
+   * Get an entity by alias. You can set an alias for an entity via the Entity object.
+   * @see Entity
+   * @param alias the alias of your entity
+   */
   getEntity(alias: string): Entity;
+
+  /**
+   * Get an entity by it's id. The id is managed by themis internally, you can get the id via the Entity object
+   * @see Entity
+   * @param entityId the id of the entity
+   */
   getEntity(entityId: number): Entity;
+
+  /**
+   * Create a new and totally blank entity
+   * @see Entity
+   */
   createEntity(): Entity;
+
+  /**
+   * Create a new entity which is based on a blueprint. You can learn more about blueprints in the documentation.
+   * @see registerBlueprint
+   * @param blueprint
+   */
   createEntity(blueprint: string): Entity;
+
+  /**
+   * Register a new blueprint. This can then be used to create new entities based on the given BlueprintDefinition
+   * @see createEntity
+   * @param blueprint
+   */
   registerBlueprint(blueprint: BlueprintDefinition): void;
+
+  /**
+   * Register an EventListener. If the given event occurs, all registered listeners be called after all Systems
+   * have done their updated. All event calls are queued and performed in the order they were called. If an error
+   * occurs, you can handle the error via the given error callback.
+   * @param eventType
+   * @param listener
+   * @param errorCallback
+   */
   registerListener<T extends Event>(
     eventType: EventType<T>,
     listener: EventListener<T>,
     errorCallback?: EventErrorCallback<T>
   ): void;
+
+  /**
+   * Submit the given event. All EventListeners will be informed about this call after all systems have updated.
+   * If you don't want your event to be queued, you can set the instant parameter to true, this will result in your
+   * event being processed directly in this call.
+   * @param eventType
+   * @param event
+   * @param instant
+   */
   submit<T extends Event>(eventType: EventType<T>, event: T, instant?: boolean): void;
+
+  /**
+   * You can use this method to inject the dependencies which were configured during world building into any
+   * arbitrary object. See the documentation about dependency injection in the world builder for more details.
+   * @see WorldBuilder
+   * @param object
+   */
   inject(object: any): void;
 }
